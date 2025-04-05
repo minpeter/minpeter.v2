e33c8447d8e4456516e5a976a5ddd25afb8f9d1c
minpeter <kali2005611@gmail.com>
2025-04-06 00:39:43 +0900

    Refactor layout styles in unstructured showcase page for improved responsiveness

--- app/[locale]/show/unstructured/page.tsx
+++ app/[locale]/show/unstructured/page.tsx
@@ -12,13 +12,8 @@ export default function Page() {
 
       <div className="h-96 w-full rounded-md bg-neutral-800 p-4 text-black">
         <div className="relative h-full w-full">
-          <div className="absolute top-0 left-[1rem] h-[calc(100%-1rem)] w-[calc(100%-1rem)] bg-neutral-600"></div>
-          <div
-            className={cn(
-              "absolute top-[1rem] right-[1rem] h-[calc(100%-1rem)] w-[calc(100%-1rem)] bg-neutral-500",
-              "p-8"
-            )}
-          >
+          <div className="absolute top-0 right-0 bottom-4 left-4 bg-neutral-600" />
+          <div className="absolute top-4 right-4 bottom-0 left-0 bg-neutral-500 p-8">
             <h1 className="text-2xl font-extrabold text-neutral-200">
               hello unstructured
             </h1>
