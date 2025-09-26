9c61e9ad7d8e1f234f2111491514a3cc7be240c9
minpeter <minpeter@friendli.ai>
2025-09-27 06:13:19 +0900

    fix: update middleware matcher to exclude media route

--- middleware.ts
+++ middleware.ts
@@ -22,9 +22,7 @@ export const config = {
   runtime: "nodejs",
   // Matcher ignoring `/_next/`, `/api/`, and files with extensions
   // ".*\\..*" 는 확장자가 있는 경우 (public의 에셋인 경우) 예외처리
-  // .well-known는 flags를 위해 예외처리
-  // media for custom font
   matcher: [
-    "/((?!api|.well-known|_next/media|_next/static|_next/image|favicon.ico|.*\\..*).*)",
+    "/((?!api|.well-known|_next/static|_next/image|favicon.ico|.*\\..*).*)",
   ],
 };
